c27b8e0648ebd8bc0686b2db27622cbc6babd65c
Mustafa <wadupbiatch@icloud.com>
2022-02-23 01:05:15 +0300

    no message

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  PitchPerfect
 //
 //  Created by Mustafa on 27.11.2021.
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder: AVAudioRecorder!
     
@@ -24,10 +24,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         stopButtonOutlet.isEnabled = false
-        recordingLabel.text = "Tap to Record"
+        
     }
     
-    //Sesi kayıt etme işlemini bu alanda yapıyoruz.
+    //MARK: - Recording Functions
     @IBAction func recordButtonPressed(_ sender: UIButton) {
         stopButtonOutlet.isEnabled = true
         recordButtonOutlet.isEnabled = false
@@ -42,6 +42,7 @@ class ViewController: UIViewController {
         try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
         
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
@@ -51,7 +52,42 @@ class ViewController: UIViewController {
     @IBAction func stopButtonPressed(_ sender: UIButton) {
         stopButtonOutlet.isEnabled = false
         recordButtonOutlet.isEnabled = true
-        recordingLabel.text = "Recording stopped"
+        recordingLabel.text = "Tap to Record"
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    //MARK: - Audio Recording Delegate
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        }else {
+            showToast(message: "Recording was not successful", seconds: 3.0)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+}
+//MARK: - Toast Message
+extension RecordSoundsViewController {
+    func showToast(message : String, seconds: Double){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .black
+        alert.view.alpha = 0.5
+        alert.view.layer.cornerRadius = 15
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
     }
 }
 
